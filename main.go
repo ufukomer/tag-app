6a2fc2c5c6e4accfc52156af3a495d6c0da44e8b
ufukomer <ufukomer@gmail.com>
2019-12-03 19:07:26 +0300

    Complete cli options

--- main.go
+++ main.go
@@ -1,37 +1,45 @@
 package main
 
 import (
-	"github.com/pkg/errors"
+	"errors"
+	"log"
+	"os"
+
 	"github.com/urfave/cli"
 )
 
 func main() {
 	app := cli.NewApp()
-
+	app.Usage = "Edit build tags of all go files in specified directory."
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "folder, f",
+			Value: ".",
+		},
+		cli.StringFlag{
+			Name: "tags, t",
+		},
+		cli.StringFlag{
+			Name:  "suffix, s",
+			Value: ".go",
+		},
+	}
+	app.Before = func(c *cli.Context) error {
+		if !c.GlobalIsSet("tags") {
+			return errors.New("tags flag is not set")
+		}
+		return nil
+	}
 	app.Commands = []cli.Command{
 		{
-			Name: "append",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "folder, f",
-					Value: ".",
-				},
-				cli.StringFlag{
-					Name: "tags, t",
-				},
-				cli.StringFlag{
-					Name:  "suffix, s",
-					Value: ".go",
-				},
-			},
-			Usage: "append build tag into all files in directory, replaces existing tags.",
+			Name:  "append, a",
+			Usage: "Appends build tag into all files in directory (replaces existing tags)",
 			Action: func(c *cli.Context) error {
-				if !c.IsSet("tags") {
-					return errors.New("tags flag is not set")
-				}
-
-				return appendTags(c.String("folder"), c.String("tags"), c.String("suffix"))
+				return appendTags(c.GlobalString("folder"), c.GlobalString("tags"), c.GlobalString("suffix"))
 			},
 		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
 }
